86ab1c5930d517896e17ac622d8fd75ef2fb3cc6
krzysztof <krzysztof@grywamtu.pl>
2018-01-07 22:21:40 +0100

    Dodaj pliki projektów.

---
+++ TS3.Bot.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TS3.Bot.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
