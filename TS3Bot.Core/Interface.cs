2395a80b92af815ad828b7382c24dbe7d42e9825
GwtLabs <kris@biggames.pl>
2018-01-11 00:59:56 +0100

    Added Interface

--- TS3Bot.Core/Interface.cs
+++ TS3Bot.Core/Interface.cs
@@ -7,6 +7,30 @@ namespace TS3Bot.Core
 {
     class Interface
     {
-        public Server Server;
+        private static readonly Interface instance = new Interface();
+
+        public static Server Server { get; private set; }
+        
+        static Interface()
+        {
+        }
+
+        public static Interface Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private Interface()
+        {
+
+        }
+
+        public void Initialize(Server server)
+        {
+            Server = server;
+        }
     }
 }
