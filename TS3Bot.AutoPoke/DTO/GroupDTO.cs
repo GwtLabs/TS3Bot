091d59656e4344cbc683924f6fbbd2ff7225a1ef
GwtLabs <kris@biggames.pl>
2018-01-13 01:41:56 +0100

    Added AutoPoke SuperGroup property.

--- TS3Bot.AutoPoke/DTO/GroupDTO.cs
+++ TS3Bot.AutoPoke/DTO/GroupDTO.cs
@@ -3,6 +3,8 @@
     public class GroupDTO
     {
         public uint Id { get; set; }
+        // wyłącza powiadomienia z kanału nawet jeśli nie otrzymuje powiadomień z niego
+        public bool SuperGroup { get; set; } = false;
         public int DelayRelative { get; set; } = 0;
         public int DelayAbsolute { get; set; } = 0;
     }
--- TS3Bot.AutoPoke/Model/StaffGroupData.cs
+++ TS3Bot.AutoPoke/Model/StaffGroupData.cs
@@ -7,6 +7,7 @@ namespace TS3Bot.Ext.AutoPoke.Model
     public class StaffGroupData
     {
         public uint Id { get; set; }
+        public bool SuperGroup { get; set; }
         public int DelayRelative { get; set; }
         public int DelayAbsolute { get; set; }
     }
